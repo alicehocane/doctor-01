2a4369c811b390f3549ff620e2c4a12e778ef7b9
alicehocane <hereyouth@gmail.com>
2025-05-27 21:53:21 +0500

    update route timeline

--- app/sitemap.doctors/[page]/route.ts
+++ app/sitemap.doctors/[page]/route.ts
@@ -3,8 +3,7 @@ import { NextResponse } from 'next/server';
 import { firestore } from '../../../lib/firebase-admin';
 
 export const dynamic = 'auto';
-export const revalidate = 604800; // 7 days
-
+export const revalidate = 60 * 60 * 24 * 30; // 30 dayss 
 /**
  * Dynamic paginated doctor sitemap.
  * @param params.page Page number of the sitemapss
